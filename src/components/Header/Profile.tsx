6628410cec17f094ec30660ed272dc4f0f087f63
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-03-20 16:41:04 -0300

    feat: implements responsive header components

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Avatar, Box, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Gabriel Lopes</Text>
-                <Text color="gray.300" fontSize="small">gabriel.lopes.nascimento.dev@gmail.com</Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Gabriel Lopes</Text>
+                    <Text color="gray.300" fontSize="small">gabriel.lopes.nascimento.dev@gmail.com</Text>
+                </Box>
+            )}
             <Avatar size="md" name="Gabriel Lopes" src="https://github.com/gabriellopes232.png" />
         </Flex>
     )
