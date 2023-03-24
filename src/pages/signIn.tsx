bd91c0aacb3a715915a1e866a1995e0f80f7eb2e
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-03-24 14:40:31 -0300

    feat: use react-hook-form to get info inputs

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,7 +1,23 @@
-import Input from '@/components/Form/Input'
-import { Button, Flex, Stack } from '@chakra-ui/react'
+//@ts-nocheck
+
+import { Input } from '@/components/Form/Input';
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type SignInFormData = {
+    email: string;
+    password: string;
+}
 
 export default function SignIn() {
+    const { register, handleSubmit, formState } = useForm();
+
+    const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        console.log(values);
+    }
+
     return (
         <Flex
             width="100vw"
@@ -15,16 +31,18 @@ export default function SignIn() {
                 bg="gray.800"
                 p="8"
                 borderRadius={8}
-                flexDir="column">
+                flexDir="column"
+                onSubmit={handleSubmit(handleSignIn)}>
                 <Stack spacing="4">
-                    <Input name="email" type="email" label="E-mail" />
-                    <Input name="password" type="password" label="Senha" />
+                    <Input name="email" type="email" label="E-mail" ref={register} />
+                    <Input name="password" type="password" label="Senha" ref={register} />
                 </Stack>
                 <Button
                     type="submit"
                     mt="6"
                     colorScheme="pink"
-                    size="lg">
+                    size="lg"
+                    isLoading={formState.isSubmitting}>
                     Entrar
                 </Button>
             </Flex>
