be4882dd3a839df4ec6637e7f88bb185ad5755a5
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-04-05 12:28:42 -0300

    feat: add errors validation

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -3,14 +3,23 @@
 import { Input } from '@/components/Form/Input';
 import { Button, Flex, Stack } from '@chakra-ui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type SignInFormData = {
     email: string;
     password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+    email: yup.string().required('E-mail obrigatório').email(),
+    password: yup.string().required('Senha obrigatória')
+})
+
 export default function SignIn() {
-    const { register, handleSubmit, formState } = useForm();
+    const { register, handleSubmit, formState, errors } = useForm({
+        resolver: yupResolver(signInFormSchema)
+    });
 
     const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
         await new Promise(resolve => setTimeout(resolve, 2000));
@@ -23,7 +32,8 @@ export default function SignIn() {
             width="100vw"
             height="100vh"
             alignItems="center"
-            justifyContent="center">
+            justifyContent="center"
+        >
             <Flex
                 as="form"
                 width="100%"
@@ -34,8 +44,8 @@ export default function SignIn() {
                 flexDir="column"
                 onSubmit={handleSubmit(handleSignIn)}>
                 <Stack spacing="4">
-                    <Input name="email" type="email" label="E-mail" ref={register} />
-                    <Input name="password" type="password" label="Senha" ref={register} />
+                    <Input name="email" type="email" error={errors.email} label="E-mail" ref={register} />
+                    <Input name="password" type="password" label="Senha" error={errors.password} ref={register} />
                 </Stack>
                 <Button
                     type="submit"
